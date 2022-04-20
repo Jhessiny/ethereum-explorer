41734119a24b014781d162f627af66dde06ea749
Jhessiny <jhessinymattos@gmail.com>
2022-04-20 12:49:29 -0300

    feat: ensure RemoteLoadBlockchainData calls HttpPostClient with correct url

---
+++ src/data/usecases/load-blockchain-data/remote-load-blockchain-data.ts
@@ -0,0 +1,8 @@
+import { HttpPostClient } from '@/data/protocols/http/http-post-client'
+
+export class RemoteLoadBlockchainData {
+  constructor (private readonly url: string, private readonly httpPostClient: HttpPostClient) {}
+  async load (): Promise<void> {
+    await this.httpPostClient.post(this.url)
+  }
+}
