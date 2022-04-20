dd2453f6785e9f74a2535e9e6ce8e9b20b597b52
Jhessiny <jhessinymattos@gmail.com>
2022-04-20 13:11:01 -0300

    refactor: change httpPostClient to receive an object

--- src/data/usecases/load-blockchain-data/remote-load-blockchain-data.ts
+++ src/data/usecases/load-blockchain-data/remote-load-blockchain-data.ts
@@ -3,6 +3,6 @@ import { HttpPostClient } from '@/data/protocols/http/http-post-client'
 export class RemoteLoadBlockchainData {
   constructor (private readonly url: string, private readonly httpPostClient: HttpPostClient) {}
   async load (): Promise<void> {
-    await this.httpPostClient.post(this.url)
+    await this.httpPostClient.post({ url: this.url })
   }
 }
