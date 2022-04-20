c38f3e18162090000f737165998699f7f3c11991
Jhessiny <jhessinymattos@gmail.com>
2022-04-20 12:57:16 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/load-blockchain-data/remote-load-blockchain-data.spec.ts
+++ src/data/usecases/load-blockchain-data/remote-load-blockchain-data.spec.ts
@@ -1,11 +1,23 @@
 import { HttpPostClientSpy } from './../../test/mock-http-client'
 import { RemoteLoadBlockchainData } from './remote-load-blockchain-data'
 
+type SutTypes = {
+  sut: RemoteLoadBlockchainData
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url: string = 'any_url'): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteLoadBlockchainData(url, httpPostClientSpy)
+  return {
+    sut, httpPostClientSpy
+  }
+}
+
 describe('RemoteLoadBlockchainData', () => {
   it('should call HttpPostClient with correct url', async () => {
     const url = 'test'
-    const httpPostClientSpy = new HttpPostClientSpy()
-    const sut = new RemoteLoadBlockchainData(url, httpPostClientSpy)
+    const { sut, httpPostClientSpy } = makeSut(url)
     await sut.load()
     expect(httpPostClientSpy.url).toBe(url)
   })
