da74feec2f1f72a1f9f576e91fb4745c0bd05613
Jhessiny <jhessinymattos@gmail.com>
2022-04-20 13:01:00 -0300

    refactor: change remoteLoadBlockchainData to use faker

--- src/data/usecases/load-blockchain-data/remote-load-blockchain-data.spec.ts
+++ src/data/usecases/load-blockchain-data/remote-load-blockchain-data.spec.ts
@@ -1,12 +1,13 @@
 import { HttpPostClientSpy } from './../../test/mock-http-client'
 import { RemoteLoadBlockchainData } from './remote-load-blockchain-data'
+import faker from 'faker'
 
 type SutTypes = {
   sut: RemoteLoadBlockchainData
   httpPostClientSpy: HttpPostClientSpy
 }
 
-const makeSut = (url: string = 'any_url'): SutTypes => {
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
   const httpPostClientSpy = new HttpPostClientSpy()
   const sut = new RemoteLoadBlockchainData(url, httpPostClientSpy)
   return {
@@ -16,7 +17,7 @@ const makeSut = (url: string = 'any_url'): SutTypes => {
 
 describe('RemoteLoadBlockchainData', () => {
   it('should call HttpPostClient with correct url', async () => {
-    const url = 'test'
+    const url = faker.internet.url()
     const { sut, httpPostClientSpy } = makeSut(url)
     await sut.load()
     expect(httpPostClientSpy.url).toBe(url)
